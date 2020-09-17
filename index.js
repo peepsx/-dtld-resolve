6b8108c5f1462547d4781914f302e27939c3bbd5
shikhars371 <shikhars371@gmail.com>
2020-09-17 14:01:52 +0530

    var changes

--- index.js
+++ index.js
@@ -1,74 +1,92 @@
-var lookup = require('@dtld/lookup');
-var events = require('events');
-var debug = require('debug')('dtld-resolve');
+import { Lookup } from '@dtld/lookup';
+import { EventEmitter } from 'events';
 
-// Keys for distributed error pages (dDrive keys)
-var DWEB_BAD_DOMAIN_HASH = null; // 800 error
-var DWEB_BAD_KEY_HASH = null; // 801 error
-var DWEB_FAIL_HASH = null; // 802 error
+// Global constants for distributed error page keys (dDrive keys)
+const DWEB_NO_DOMAIN_HASH = null; //800 error
+const DWEB_BAD_KEY_HASH = null; // 801 error
+const DWEB_BAD_DOMAIN_HASH = null; // 802 error
 
-// Validation for two-level and three-level domains (peepsx.dcom and www.peepsx.dcom)
-var TWO_LEVEL_REGEX = /^(([a-zA-Z0-9]\\.)|([a-zA-Z0-9][-a-zA-Z0-9]{0,62}[a-zA-Z0-9]\\.) ?)[a-zA-Z0-9]{2,6}$/;
-var THREE_LEVEL_REGEX = /^(([a-zA-Z0-9]\\.)|(a-zA-Z0-9][-a-zA-Z0-9]{0,62}[a-zA-Z0-9]\\.){2})[a-zA-Z0-9]{2,6}$/;
+// Regular expressions for two or three-level domains (peepsx.dcom and www.peepsx.dcom)
+const THREE_LEVEL_REGEX = /^[a-z][a-z0-9-]+[a-z0-9]\.[a-z][a-z0-9-]+a-z0-9]+(?:\.dcom|\.dnet|\.dorg|\.dgov|\.follow|\.dweb|\.dinfo)/i;
+const TWO_LEVEL_REGEX = /^[a-z][a-z0-9-]+[a-z0-9](?:\.dcom|\.dnet|\.dorg|\.dgov|\.follow|\.dweb|\.dinfo)/i;
 
-// Validation for dWeb keys
-var DWEB_HASH_REGEX = /^[0-9a-f]{64}?$/i;
+// Regular Expression patterns for the dWeb protocol and dWeb keys
+const DWEB_PROTOCOL_REGEX = /^dweb:\/\//i;
+const DWEB_HASH_REGEX = /^[0-9a-f]{64}?$/i;
 
-module.exports = ResolveDWebKey;
+// TODO: Fix DRY code
+class Resolve extends EventEmitter {
+  constructor(dtld) {
+    super();
+    this.dtld = dtld;
+  }
+  
+  getKey() {
+    let search = this;
+    let name = search.dtld;
+    return new Promise( (resolve, reject) => {
+      //remove dWeb protocol prefix from domain (if exists)
+      if(name.test(DWEB_PROTOCOL_REGEX)) {
+        name = name.replace(DWEB_PROTOCOL_REGEX, "");
+      }
+      let convertDomain = name.toLowerCase();
+      let parsedDomain = convertDomain.split(".");
+      if(name.test(THREE_LEVEL_REGEX) && parsedDomain.length === 3) {
+        //separate record name and domain into different definitions
+        let record_name = parsedDomain[0], domain_key = parsedDomain[1], dtld_type = parsedDomain[2];
+        let domain = domain_key + "." + dtld_type;
+        // Lookup domain and record_name on ARISEN
+        let lookup = new Lookup(domain, record_name);
+        lookup.on('found', key => {
+          if (key.test(DWEB_HASH_REGEX)) {
+            search.emit('resolved', key);
+            return resolve();
+          }  else if (!key.test(DWEB_HASH_REGEX)) {
+            search.emit('801', DWEB_BAD_KEY_HASH);
+            return reject(new Error("Resolved dWeb key is not a valid dWeb key."));
+          }
+        });
+        lookup.on('none', () => {
+          search.emit('800', DWEB_NO_DOMAIN_HASH);
+          return reject(new Error("dTLD or record for dTLD does not exist."));
+        });
+        lookup.go()
+          .then( () => console.log("Key resolved successfully"))
+          .catch( err => {
+            console.error(err.message);
+          });
+      }  else if (name.test(TWO_LEVEL_REGEX) && parsedDomain.length === 2) {
+        //two-level dTLDs always have "root" as the record name
+        let record_name = "root", domain_key = parsedDomain[0], dtld_type = parsedDomain[1];
+        let domain = domain_key + "." + dtld_type;
+        // Lookup dWeb key by domain and "root" record on ARISEN
+        let lookup = new Lookup(domain, record_name);
+        lookup.on('found', key => {
+          if (key.test(DWEB_HASH_REGEX)) {
+            search.emit('resolved', key);
+            return resolve();
+          }  else if (!key.test(DWEB_HASH_REGEX)) {
+            search.emit('801', DWEB_BAD_KEY_HASH);
+            return reject(new Error("Resolved dWeb key is not a valid dWeb key."));
+          }
+        }); // end found listener
+        lookup.on('none', () => {
+          search.emit('800', DWEB_NO_DOMAIN_HASH);
+          return reject(new Error("dTLD or record does not exist."));
+        });
+        lookup.go()
+          .then( () => console.log("Key resolved successfully"))
+          .catch( err => {
+            console.error(err.message);
+          });
+      } else if (!name.test(THREE_LEVEL_REGEX) && (!name.test(TWO_LEVEL_REGEX))) {
+        search.emit('802', DWEB_BAD_DOMAIN_HASH);
+        return reject(new Error("Domain is not a valid two or three-level domain."));
+      } else {
+        return reject(new Error("Unknown error."));
+      }
+    }); //end returned Promise
+  } // end getKey method
+} // end Resolve class
 
-function ResolveDWebKey(dtld) {
-    var convertDomain = dtld.toLowerCase(); // convert dTLD to lowercase string
-    var parsedDomain = convertDomain.split("."); // create array of dTLD segments between periods
-    // if dTLD array has 3 elements and dTLD matches three-level domain regex
-    if ( dtld === THREE_LEVEL_REGEX && parsedDomain.length === 3 ) {
-
-        //separate record_name and domain into different variables 
-        var record_name = parsedDomain[0];
-        var domain_key = parsedDomain[1];
-        var dtld_type = parsedDomain[2];
-        var domain = domain_key + "." + dtld_type;
-        
-        // lookup dWeb key by domain and record_name on ARISEN
-        var dweb_key = lookup(domain, record_name);
-
-        if ( dweb_key === false ) {
-            return DWEB_BAD_DOMAIN_HASH;
-        }
-        else if ( dweb_key === DWEB_HASH_REGEX ) {
-            return dweb_key;
-        }
-        else if ( dweb_key != DWEB_HASH_REGEX ) {
-            return DWEB_BAD_KEY_HASH;
-        }
-        else {
-            return DWEB_FAIL_HASH;
-        }
-    }
-    else if ( dtld === TWO_LEVEL_REGEX && parsedDomain.length === 2 ) {
-        //get domain
-        var record_name = "root"; // two-level dTLDs always use "root" record names
-        var domain_key = parsedDomain[0];
-        var dtld_type = parsedDomain[1];
-        var domain = domain_key + "." + dtld_type;
-
-        // lookup dWeb key by domain and record_name on ARISEN
-        var dweb_key = lookup(domain, record_name);
-
-        if (  dweb_key === false ) {
-            return DWEB_BAD_DOMAIN_HASH;
-        }
-        else if ( dweb_key === DWEB_HASH_REGEX ) {
-            return dweb_key;
-        }
-        else if ( dweb_key != DWEB_HASH_REGEX ) {
-            return DWEB_BAD_KEY_HASH;
-        }
-        else {
-            return DWEB_FAIL_HASH;
-        }
-    }
-    else {
-        // Must be a bad domain or invalid domain - return bad domain hash
-        return DWEB_BAD_DOMAIN_HASH;
-    }
-}
+export default Resolve;
